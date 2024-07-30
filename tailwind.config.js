5d860300000265d007377deb818439561f8da5e0
omarnate1 <omarnate@gmail.com>
2024-07-30 08:58:27 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html", "./*.js"],
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         "light-pink": "#f9f0ff",
         "grayish-purple": "#8c6991",
-        "dark-purple": "#3e1d54", // You might want to add the exact dark purple color code
+        "dark-purple": "#3e1d54",
       },
       fontFamily: {
         "work-sans": ["Work Sans", "sans-serif"],
